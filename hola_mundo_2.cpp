ab1f23005ac15b033a39fb1842a4f78c4b15ae68
Adrian_Isaac_Sihues_Diaz <sihuesadrian@gmail.com>
2026-01-29 13:42:49 -0400

    sexto commit intento tres de guardado githud

---
+++ hola_mundo_2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "¡Hola Mundo este es mi segundo intento en git"<< endl;
+    cout << "Mi nombre es: Adrian" << endl;
+    cout << "Mi apellido es: Sihues" << endl;
+    cout << "Estoy aprendiendo Git en Programación II" << endl;
+    cout << "Este es mi quinto commit" << endl;
+    return 0;
+}
